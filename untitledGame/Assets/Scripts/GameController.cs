fa99b6dca5e33d704705ade06771c3d83cf82576
Nick <Nick14.clothier@gmail.com>
2020-12-03 20:14:13 +0000

    Started Creating Board Game
    Started Created Board Game and Player Movement.
    - Created Basic Player Movement Scripts.
      {Needs Fixing For Smooth Transition)
    - Created Array Of Waypoints around a board game that the player can move to each one.
    - Created Button Which Randomly Rolls the die, Moving the player that amount.

---
+++ untitledGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Button RollButton;
+    public Text TextDie;
+    public GameObject Player1;
+
+    public int Die = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = RollButton.GetComponent<Button>();
+        Text txt = TextDie.GetComponent<Text>();
+        btn.onClick.AddListener(RollDice);
+    }
+
+    public void RollDice()
+    {
+        Die = Random.Range(1, 7);
+        TextDie.text = "Number is " + Die;
+        Player1.GetComponent<PlayerMovement>().Moves = Die;
+        Player1.GetComponent<PlayerMovement>().MoveAllowed = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ untitledGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform[] WayPoint;
+    public int WayPointNumber = 0;
+    public float MoveSpeed = 1f;
+    public bool MoveAllowed = false;
+    public int Moves = 0;
+
+    void Start()
+    {
+        transform.position = WayPoint[WayPointNumber].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (MoveAllowed == true)
+        {
+            MovePlayer();
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            MoveAllowed = true;
+
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            MoveAllowed = false;
+        }
+    }
+
+    public void MovePlayer()
+    {
+        for (int i = 0; i <= Moves - 1;)
+        {
+            if (WayPointNumber <= WayPoint.Length - 1)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, WayPoint[WayPointNumber].transform.position, MoveSpeed * Time.deltaTime);
+
+                if (transform.position == WayPoint[WayPointNumber].transform.position)
+                {
+                    WayPointNumber += 1;
+                    i++;
+                }
+            }
+
+        }
+        MoveAllowed = false;
+
+        //if (WayPointNumber <= WayPoint.Length - 2)
+        //{
+        //    WayPointNumber = WayPointNumber + 1;
+
+
+        //    //Glides To Each One.
+        //    //transform.position = Vector3.MoveTowards(transform.position, WayPoint[WayPointNumber].transform.position, MoveSpeed * Time.deltaTime);
+
+
+        //}
+        //else
+        //{
+        //    WayPointNumber = 0;
+        //}
+        ////transform.position = WayPoint[WayPointNumber].transform.position;
+        //transform.position = Vector3.MoveTowards(transform.position, WayPoint[WayPointNumber].transform.position, MoveSpeed * Time.deltaTime);
+    }
+}
