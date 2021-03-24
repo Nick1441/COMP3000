88acc3c0a46ca30a39930e5b4f085c972e77b9b3
Nick1441 <Nick14.clothier@gmail.com>
2021-03-24 05:16:49 +0000

    Started Creating MiniGame1
    Started creating a remix of Bomberman, as this will be my first Minigame.
    - Created a movement script for 2 players.
    - When space is pressed, creates a bomb, which then lasts 3 seconds, then will spawn a small cube in for 0.5. This will later be replaced by custom objects of a bomb and a particle effect for an explosion.

---
+++ untitledGame/Assets/Scripts/MiniGame1/BombExplosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombExplosion : MonoBehaviour
+{
+
+    public GameObject explosionPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Trigger", 3.0f);
+    }
+
+    // Update is called once per frame
+    void Trigger()
+    {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity); //1
+
+        GetComponent<MeshRenderer>().enabled = false;
+        Destroy(gameObject, .3f); //4
+
+    }
+}
---
+++ untitledGame/Assets/Scripts/MiniGame1/BombManMovement.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BombManMovement : MonoBehaviour
+{
+    public int playerNumber = 1;
+    public float moveSpeed = 5f;
+    public bool canDropBombs = true;
+    public bool canMove = true;
+
+    private int bombs = 2;
+    public GameObject bombPrefab;
+
+    private Rigidbody rigidBody;
+    private Transform myTransform;
+    private Animator animator;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        if (playerNumber == 1)
+        {
+            UpdatePlayer1Movement();
+        }
+        else
+        {
+            UpdatePlayer2Movement();
+        }
+    }
+
+    private void UpdatePlayer1Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.velocity = new Vector3(-moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.velocity = new Vector3(moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (canDropBombs && Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnBomb();
+        }
+    }
+
+    private void UpdatePlayer2Movement()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.velocity = new Vector3(-moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 270, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -moveSpeed);
+            myTransform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.velocity = new Vector3(moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            myTransform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+
+        if (canDropBombs && (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)))
+        {
+            SpawnBomb();
+        }
+    }
+
+    private void SpawnBomb()
+    {
+            Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x), 0.5f, Mathf.RoundToInt(myTransform.position.z)), bombPrefab.transform.rotation);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Explosion"))
+        {
+            Debug.Log("P" + playerNumber + " hit by explosion!");
+        }
+    }
+}
